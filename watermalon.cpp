774b20c5e4b4a0057328c97521dfb7dc605822d6
Uday Gohel <114012274+UdayGohel@users.noreply.github.com>
2022-11-28 14:07:02 +0530

    Add files via upload

---
+++ watermalon.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int a;
+    cin>>a;
+    
+    if(a%2==1 || a==2){
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        cout<<"YES"<<endl;
+    }
+
+    return 0;
+}
