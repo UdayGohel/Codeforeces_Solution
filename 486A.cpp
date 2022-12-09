9606deaf2a506ea41bf4d9343ea7a1110180a05e
Uday Gohel <114012274+UdayGohel@users.noreply.github.com>
2022-12-09 21:58:13 +0530

    Add files via upload

---
+++ 486A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   long long int a,sum=0;
+    cin>>a;
+        if(a%2==0)
+        {
+            cout<<a/2<<endl;
+        }
+        else
+        {
+           cout<<-(a+1)/2<<endl;
+        }
+}
